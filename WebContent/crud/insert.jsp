843060ef0fd1580dc91edf3ef183d7c5bc49fd3d
administrador <administrador@modelo>
2022-09-15 13:46:07 -0300

    Update style.css, delete.jsp, and 5 more files...

---
+++ WebContent/crud/insert.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Insert</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<body>
+
+	<%@include file="../includes/header.jsp"%>
+
+	<div id="wrapper">
+		<main>
+			<div id="content">
+				<div class="innertube">
+					<h1>Insert</h1>
+
+				</div>
+			</div>
+		</main>
+
+		<%@include file="../includes/menu.jsp"%>
+	</div>
+
+	<%@include file="../includes/footer.jsp"%>
+</body>
+
+</html>
---
+++ sql/Dump.sql
@@ -0,0 +1,19 @@
+create table if not exists 'Student'(
+	id int(11),
+	name varchar(100),
+	city varchar(100),
+	phone varchar(20)
+	Engine=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+);
+
+insert into student (id, name, city, phone) values (1,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (2,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (3,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (4,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (5,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (6,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (7,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (8,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (9,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (10,'sergio','são paulo','12345678');
+insert into student (id, name, city, phone) values (11,'sergio','são paulo','12345678');
